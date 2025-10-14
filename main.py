282da3a78cab197e246656e4cd1e609b010c901f
Paulo Daniel <92828125+kampello@users.noreply.github.com>
2025-10-14 14:37:22 +0100

    Adicinoi a funcao de ler floats

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from get_int import *
+from get_float import*
 
 if __name__ == '__main__':
-    x = get_int("x: ")
-    y = get_int("y: ")
-    z = x+y
+        x = get_float("x: ")
+        i = get_int("i: ")
+        z = x*i
 
-    print(z)
+        print(z)
