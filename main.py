1f087b32c64916b3a11be4b348834d2ec0be68b9
Paulo Daniel <92828125+kampello@users.noreply.github.com>
2025-10-21 15:13:58 +0100

    novo print added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from utils import *
-
+#teste de input
 if __name__ == '__main__':
     x = get_float("x: ")
     i = get_int("i: ")
     z = x*i
 
     print(z)
+    print("TEST PRINT PARA TESTE DE GITHUB")
